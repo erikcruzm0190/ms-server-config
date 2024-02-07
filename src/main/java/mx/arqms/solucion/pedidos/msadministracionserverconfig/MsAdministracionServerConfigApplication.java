4eeea31e4467eac37afc535442c194ed830421a3
Erik Cruz <ecruz@ipcom.com.mx>
2024-02-06 19:35:48 -0600

    add project

---
+++ src/main/java/mx/arqms/solucion/pedidos/msadministracionserverconfig/MsAdministracionServerConfigApplication.java
@@ -0,0 +1,15 @@
+package mx.arqms.solucion.pedidos.msadministracionserverconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MsAdministracionServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAdministracionServerConfigApplication.class, args);
+	}
+
+}
